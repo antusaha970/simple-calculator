c24d5a2108c895f610f385151604b18005305dcf
antu <antusaha970@gmail.com>
2022-12-20 21:08:12 +0600

    power  fulctionality added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 let inputStr = "";
+let mainExpression = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', function (event) {
         if (event.target.innerText == "=") {
-            inputStr = eval(inputStr);
+            inputStr = eval(mainExpression);
+            mainExpression = eval(mainExpression);
         }
         else if (event.target.innerText == "AC") {
             inputStr = "";
+            mainExpression = "";
+        }
+        else if (event.target.innerText == "^") {
+            inputStr = inputStr + event.target.innerText;
+            mainExpression = mainExpression + "**";
         }
         else {
             inputStr = inputStr + event.target.innerText;
+            mainExpression = mainExpression + event.target.innerText;
         }
         document.querySelector('.input').value = inputStr;
     })
